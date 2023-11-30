2d75732fcbe2910f211cd2242686893ad885b3a1
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-12-01 05:42:38 +0700

    update db

--- db/migration-golang/20231127065301_create_topup_tabel.up.sql
+++ db/migration-golang/20231127065301_create_topup_tabel.up.sql
@@ -1,11 +1,11 @@
 BEGIN;
 CREATE TABLE IF NOT EXISTS "public"."topup" (
     "id" varchar(255) NOT NULL PRIMARY KEY,
-    "user_id" integer NOT NULL,
-    "amount" integer NOT NULL,
-    "status" integer default 0,
-    "snap_url" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL,
-    "updated_at" timestamp NOT NULL
+    "user_id" INT,
+    "amount" INT,
+    "status" INT,
+    "snap_url" varchar(255),
+    "created_at" TIMESTAMP,
+    "updated_at" TIMESTAMP
 );
 COMMIT;

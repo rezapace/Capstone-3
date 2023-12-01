3a8c94a141f2ff90b55adf24724ba0fc0548096d
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-12-02 00:19:06 +0700

    update saldo

--- entity/tiket.go
+++ entity/tiket.go
@@ -5,7 +5,6 @@ import (
 )
 
 type Ticket struct {
-<<<<<<< HEAD
 	ID          int64  `json:"id"`
 	Image       string `json:"image"`
 	Location    string `json:"location"`
@@ -23,25 +22,6 @@ type Ticket struct {
 }
 
 func NewTicket(image, location, date, title, description, category string, price, quota, terjual int64) *Ticket {
-=======
-	ID          int64     `json:"id"`
-	Image       string    `json:"image"`
-	Location    string    `json:"location"`
-	Date        string    // Format: YYYY-MM-DD
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Price       int64     `json:"price"`
-	Status      string    `json:"Status"` // e.g., 'available', 'sold out'
-	Quota       int64     `json:"Quota"`
-	Category    string    `json:"category"`
-	Tersisa     int64     `json:"Tersisa"` // e.g., 1000, 5000, 10000
-	CreatedAt   time.Time `json:"-"`
-	UpdatedAt   time.Time `json:"-"`
-	DeletedAt   time.Time `json:"-"`
-}
-
-func NewTicket(image, location, date, title, description, category, status string, price, quota, tersisa int64) *Ticket {
->>>>>>> b33534bafa33122c7febb55153d3fad3cbcb800a
 	return &Ticket{
 		Image:       image,
 		Location:    location,
@@ -50,7 +30,6 @@ func NewTicket(image, location, date, title, description, category, status strin
 		Description: description,
 		Category:    category,
 		Price:       price,
-		Status:      status,
 		Quota:       quota,
 		Terjual:     terjual,
 	}

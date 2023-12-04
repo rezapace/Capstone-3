f3a0b3b0c1a71fe1ca357bb00657c59762839f79
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-12-05 05:14:29 +0700

    update ai

--- internal/http/handler/user_hendler.go
+++ internal/http/handler/user_hendler.go
@@ -316,6 +316,49 @@ func (h *UserHandler) UserLogout(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, map[string]string{"success": "successfully logged out"})
 }
 
+// func DiagnosticAI(userInput, openAIKey string) (string, error) {
+// 	ctx := context.Background()
+// 	client := openai.NewClient(openAIKey)
+// 	model := openai.GPT3Dot5Turbo
+// 	messages := []openai.ChatCompletionMessage{
+// 		{
+// 			Role:    openai.ChatMessageRoleSystem,
+// 			Content: "Siswa menanyakan soal. Jawablah dengan materi tentang soal dan jangan berikan jawaban secara langsung",
+// 		},
+// 		{
+// 			Role:    openai.ChatMessageRoleUser,
+// 			Content: userInput,
+// 		},
+// 	}
+
+// 	resp, err := getCompletionFromMessages(ctx, client, messages, model)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	answer := resp.Choices[0].Message.Content
+// 	return answer, nil
+// }
+
+// func getCompletionFromMessages(
+// 	ctx context.Context,
+// 	client *openai.Client,
+// 	messages []openai.ChatCompletionMessage,
+// 	model string,
+// ) (openai.ChatCompletionResponse, error) {
+// 	if model == "" {
+// 		model = openai.GPT3Dot5Turbo
+// 	}
+
+// 	resp, err := client.CreateChatCompletion(
+// 		ctx,
+// 		openai.ChatCompletionRequest{
+// 			Model:    model,
+// 			Messages: messages,
+// 		},
+// 	)
+// 	return resp, err
+// }
+
 // TanyaAI
 // func (h *UserHandler) TanyaAI(c echo.Context) error {
 // 	// Baca data masukan dari body request
